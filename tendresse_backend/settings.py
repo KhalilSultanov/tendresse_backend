2c66b2d22e499c711f3e509e620daf30a96763aa
KhalilSultanov <334332@niuitmo.ru>
2024-04-20 01:42:15 +0300

    realized: - [x] STATIC_URL = '/static/'
    STATICFILES_DIRS = [
        os.path.join(BASE_DIR, 'static/blog_photos'),
        os.path.join(BASE_DIR, 'static/category_photos'),
        os.path.join(BASE_DIR, 'static/products_photos'),
        # ... другие папки с статическими файлами, если они есть ...
    ]
    
    STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static/')

--- tendresse_backend/settings.py
+++ tendresse_backend/settings.py
@@ -133,12 +133,16 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
-STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static/')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/blog_photos'),
+    os.path.join(BASE_DIR, 'static/category_photos'),
+    os.path.join(BASE_DIR, 'static/products_photos'),
+    # ... другие папки с статическими файлами, если они есть ...
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
